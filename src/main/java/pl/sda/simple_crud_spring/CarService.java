ec21a9a2da19efd0f0195c99124807ec2ea960fe
Bartlomiej Janik <yanekk1990@gmail.com>
2020-10-25 13:00:04 +0100

    CarService.java

--- src/main/java/pl/sda/simple_crud_spring/CarService.java
+++ src/main/java/pl/sda/simple_crud_spring/CarService.java
@@ -1,8 +1,18 @@
 package pl.sda.simple_crud_spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CarService {
+
+    @Autowired
+    private CarRepository carRepository;
 
     public void addCar(CarDTO carDTO){
 
+        Car car = Car.apply(carDTO);
+        carRepository.save(car);
+        System.out.println("zapisano Furę!");
     }
 }
